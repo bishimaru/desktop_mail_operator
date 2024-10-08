1140477cbafe5505fafbdf50a9b4dc7ba52ee0f9
bishimaru <a414510nee@yahoo.co.jp>
2024-10-08 23:01:44 +0900

    auto commit

--- widget/func.py
+++ widget/func.py
@@ -49,6 +49,7 @@ def get_driver(headless_flag):
       options.add_argument('--headless')
       options.add_argument("--disable-gpu")  # headlessモードの時はこのオプションを追加
     options.add_argument("--incognito")
+    options.add_argument('--log-level=3')  # これでエラーログが抑制されます
     options.add_argument('--disable-web-security')
     options.add_argument("--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1")
     options.add_argument("--no-sandbox")
