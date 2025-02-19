aa721fc399b1d801551cb84c1653614e7a96cb6a
bishimaru <a414510nee@yahoo.co.jp>
2025-02-19 20:38:05 +0900

    auto commit

--- chack_mail.bat
+++ chack_mail.bat
@@ -1,6 +1,10 @@
 @echo off
 :loop
 python check_mail.py
+if %ERRORLEVEL% NEQ 0 (
+    echo スクリプトがエラーで終了しました。エラーログを記録します...
+    echo %DATE% %TIME% - スクリプトがエラーで終了しました。 >> error_log.txt
+)
 echo スクリプトが終了しました。再起動します...
-timeout /t 5 >nul
+choice /t 180 /d y /n >nul
 goto loop
